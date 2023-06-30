f44133fc372a5d42992fda7043f33dd30bcdd28c
garima-mahato <m.garima318@gmail.com>
2023-07-01 00:33:32 +0530

    Update data_augmenter.py

--- gmo/data_engine/data_augmenter.py
+++ gmo/data_engine/data_augmenter.py
@@ -14,10 +14,10 @@ class AlbumentationTransformations():
     self.stdevs = numpy.array(stdevs)
     patch_size = 32#28
     self.album_transforms = Compose([
-      PadIfNeeded(min_height=40, min_width=40),
-      RandomSizedCrop((patch_size,patch_size), patch_size,patch_size),
       HorizontalFlip(p = 0.5),
-	  Cutout(num_holes=1, max_h_size=8, max_w_size=8, fill_value=self.means*255.0, p=0.75),
+      ShiftScaleRotate(p=0.5),
+      CoarseDropout (max_holes = 1, max_height=16, max_width=16, min_holes = 1, min_height=16, min_width=16, fill_value=self.means, mask_fill_value = None),
+	    # Cutout(num_holes=1, max_h_size=8, max_w_size=8, fill_value=self.means*255.0, p=0.75),
       Normalize(mean=self.means, std=self.stdevs),
       ToTensor()
     ])
