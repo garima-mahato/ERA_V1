b28ad24378d219d4100de354b4d9aa6501bafb3a
garima-mahato <m.garima318@gmail.com>
2023-10-25 00:12:39 +0530

    Added Session 21 assignment

---
+++ Session21_GPTandBeyond/app.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import gradio as gr
+from model import *
+
+device = 'cpu'
+model = GPTLanguageModel()
+model = model.to(device)
+model.load_state_dict(torch.load("tiny_gpt.pth", map_location=torch.device('cpu')))
+
+def generate_weird_tale(context = None, max_new_tokens = 500):
+    if context is None:
+        context = torch.zeros((1, 1), dtype=torch.long, device=device)
+    else:
+        context = torch.unsqueeze(torch.tensor(encode(context), dtype=torch.long, device=device),0)
+    #print(context.shape)
+    wtale = decode(model.generate(context, max_new_tokens=max_new_tokens)[0].tolist())
+    
+    return wtale
+
+title = "Shakespeare's Weird Tales"
+description = "When GPT fancy dresses itself as Shakespeare using 40 min of T4 GPU"
+examples = [[None,500],["Hi",1000]]
+
+context = gr.TextArea(value=None, label="Do you want to give a context ?")
+max_new_tokens = gr.Slider(1, 10000, value = 500, step=1, label="How long should the tale be (in characters)?")
+wtale = gr.TextArea(label="Your weird tale")
+
+inps = [
+      context,
+      max_new_tokens
+  ]
+
+output = [
+  wtale
+]
+
+demo = gr.Interface(
+    generate_weird_tale, 
+    inputs = inps, 
+    outputs = output,
+    title = title,
+    description = description,
+    examples = examples,
+)
+demo.launch()
